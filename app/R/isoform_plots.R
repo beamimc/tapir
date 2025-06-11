488884496545941f52abd2aa0be77ff1bf9a8e5e
Beatriz Campillo Minano (beacm) <beacm@c0300ood18.ll.unc.edu>
2025-06-10 22:09:17 -0400

    remove sign in transcript names

--- app/R/isoform_plots.R
+++ app/R/isoform_plots.R
@@ -1,5 +1,5 @@
 
-plot_isoforms_wiggle <- function(gene_of_interest, sig_res){
+plot_isoforms_wiggle <- function(exons, gene_of_interest, sig_res){
   sig_res <- sig_res %>%
     mutate(
       score = (1 - empirical_pval) * sign(estimates),  # Compute the score
@@ -10,28 +10,10 @@ plot_isoforms_wiggle <- function(gene_of_interest, sig_res){
   #tx from the gene present in the se
   # get the sig tx 
   sig_tx <- sig_res |> filter(symbol== gene_of_interest) |> select(isoform_id)
-  
-  exons_toshow <-  exons[sig_tx$isoform_id]
 
+  exons_toshow <-  exons[sig_tx$isoform_id]
   
-  # make a named vector of signs
-  signs <- setNames(sig_res$sign, sig_res$isoform_id)
-  # pull out the signs in the same order as our GRangesList names
-  the_signs <- signs[names(exons_toshow)]
-  
-  cat("Names of exons_toshow:", names(exons_toshow), "\n")
-  cat("the_signs:", the_signs, "\n")
-  
-  
-  # build the new names
-  new_names <- ifelse(
-    !is.na(the_signs) & the_signs == 1,
-    paste0("+", names(exons_toshow)),
-    paste0("-", names(exons_toshow))
-  )
-  # assign back
-  names(exons_toshow) <- new_names
-  
+  ## create transcript_annotation for coloring. needs at least transcript_id and strand
   transcript_annotations <- tibble(
     transcript_id = names(exons_toshow),
     strand = purrr::map_chr(exons_toshow, function(gr) {
@@ -46,14 +28,9 @@ plot_isoforms_wiggle <- function(gene_of_interest, sig_res){
   )
   
   transcript_annotations <- transcript_annotations %>%
-    mutate(
-      transcript_id_aux = sub("^[+-]", "", transcript_id)  # Clean for joining
-    )
-  
-  transcript_annotations <- transcript_annotations %>%
     left_join(
       sig_res %>% select(isoform_id, computed_color),
-      by = c("transcript_id_aux" = "isoform_id")
+      by = c("transcript_id" = "isoform_id")
     ) %>%
     mutate(
       color_by = ifelse(is.na(computed_color), "#F6BD60", computed_color)
@@ -74,14 +51,13 @@ plot_isoforms_wiggle <- function(gene_of_interest, sig_res){
   ggplotly(p2)
 }
 
-plot_downreg_exons <- function(gene_of_interest, sig_res, downreg_exons){
+plot_downreg_exons <- function(exons, gene_of_interest, sig_res, downreg_exons){
   sig_res <- sig_res %>%
     mutate(
-      score = (1 - empirical_pval) * sign(estimates),  # Compute the score
-      computed_color = custom_pal(score)               # Apply your custom palette
+      score = (1 - empirical_pval) * sign(estimates),  # Compute coloring score
+      computed_color = custom_pal(score)               # Apply custom palette
     )
-  
-  #tx from the gene present in the se
+
   # get the sig tx 
   sig_tx <- sig_res |> filter(symbol== gene_of_interest) |> select(isoform_id)
   
@@ -90,19 +66,6 @@ plot_downreg_exons <- function(gene_of_interest, sig_res, downreg_exons){
   
   exons_toshow <-  exons[sig_tx$isoform_id]
   
-  
-  # make a named vector of signs
-  signs <- setNames(sig_res$sign, sig_res$isoform_id)
-  # pull out the signs in the same order as our GRangesList names
-  the_signs <- signs[names(exons_toshow)]
-  # build the new names
-  new_names <- ifelse(
-    !is.na(the_signs) & the_signs == 1,
-    paste0("+", names(exons_toshow)),
-    paste0("-", names(exons_toshow))
-  )
-  # assign back
-  names(exons_toshow) <- new_names
   
   filtered <- downreg_exons %>%
     filter(gene == gene_id[[1]])
@@ -125,15 +88,11 @@ plot_downreg_exons <- function(gene_of_interest, sig_res, downreg_exons){
     })
   )
   
-  transcript_annotations <- transcript_annotations %>%
-    mutate(
-      transcript_id_aux = sub("^[+-]", "", transcript_id)  # Clean for joining
-    )
   
   transcript_annotations <- transcript_annotations %>%
     left_join(
       sig_res %>% select(isoform_id, computed_color),
-      by = c("transcript_id_aux" = "isoform_id")
+      by = c("transcript_id" = "isoform_id")
     ) %>%
     mutate(
       color_by = ifelse(is.na(computed_color), "#F6BD60", computed_color)
